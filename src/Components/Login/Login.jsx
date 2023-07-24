350930798f90370398b99ad6c5f34a6ad1d0aa2a
aayush <ayushwasnik999@gmail.com>
2023-07-25 01:59:48 +0530

    set firebase

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Login.module.css";
 import { Link } from "react-router-dom";
+import { auth } from "../../firebase";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signin = (e) => {
+    e.preventDefault();
+  };
+
+  const register = (e) => {
+    e.preventDefault();
+    auth.createUserWithEmailAndPassword(email, password);
+  };
+
   return (
     <div className={styles.login}>
       <Link to="/">
@@ -18,12 +31,24 @@ function Login() {
 
         <form>
           <h5>Email</h5>
-          <input type="text" placeholder="Enter Email" />
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Enter Email"
+          />
 
           <h5>Password</h5>
-          <input type="password" placeholder="Enter Password" />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Enter Password"
+          />
 
-          <button className={styles.sign_btn}>Sign In</button>
+          <button className={styles.sign_btn} type="submit" onClick={signin}>
+            Sign In
+          </button>
         </form>
         <p>
           By continuing, you agree to Amazon's &nbsp;
@@ -36,7 +61,7 @@ function Login() {
           </a>
           .
         </p>
-        <button className={styles.create_btn}>
+        <button onClick={register} className={styles.create_btn}>
           Create your Amazon account
         </button>
       </div>
---
+++ src/firebase.js
@@ -0,0 +1,22 @@
+import firebase from "firebase/compat/app";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBPen3apLPs1PU2o1Xa0o7xqczkW1TJ8FQ",
+  authDomain: "clone-67d4c.firebaseapp.com",
+  projectId: "clone-67d4c",
+  storageBucket: "clone-67d4c.appspot.com",
+  messagingSenderId: "1038493226694",
+  appId: "1:1038493226694:web:01c698fe7f28c1922fe9c7",
+  measurementId: "G-T04H60C6NF",
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const db = getFirestore();
+
+const auth = getAuth();
+
+export { db, auth };
